0c3dbafd211e4cf10e30cd3c89250b692c94ada7
psoni01 <91908110+psoni01@users.noreply.github.com>
2022-10-23 15:11:25 -0700

    update server side

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import express from "express";
 import http from "http";
 import {typeDefs, resolvers } from './src/schema';
 
-const startApolloServer = async ({typeDefs, resolvers}) => {
+const startApolloServer = async (typeDefs, resolvers) => {
     const app = express();
 
     const httpServer = http.createServer(app);
